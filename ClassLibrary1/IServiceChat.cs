25f74999f4dcf29c63bfbb002b7fcc6ce4062a70
MSI-90 <s.miller_22@outlook.com>
2026-04-18 01:29:15 +0700

    Инициализировать новый проект. Создать WCF сервис для начала разработки.

---
+++ ClassLibrary1/IServiceChat.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceModel;
+
+namespace ClassLibrary1
+{
+  // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+  [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+  public interface IServiceChat
+  {
+    [OperationContract]
+    int Connect();
+
+    [OperationContract]
+    void Disconnect(int id);
+
+    [OperationContract(IsOneWay = true)]
+    void SendMsg(string msg);
+  }
+
+  public interface IServerChatCallback
+  {
+    [OperationContract]
+    void MsgCallBack(string msg);
+  }
+}
